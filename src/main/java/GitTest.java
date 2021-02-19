ba905ac458d6913827e1ac6eb18d2ce44778ef5c
YuexiuPing <51785389+YuexiuPing@users.noreply.github.com>
2021-02-19 23:06:00 +0800

    githubTest

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("版本2");
         System.out.println("hot-fix");
         System.out.println("master");
+        System.out.println("github");
     }
 }
