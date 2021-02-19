fb1c870616184c9ce43c4a77e33357ac2dd37436
YueXP <YueXP@atguigu.com>
2021-02-19 22:50:07 +0800

    first commit

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,11 @@
+/**
+ * @Auther: andy
+ * @Date: 2021/2/19 - 02 - 19 - 22:48
+ * @Description: PACKAGE_NAME
+ * @version: 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
